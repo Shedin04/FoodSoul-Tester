535ce490dd7f67f3c86d34619c6e599a2a859f46
Shedin <dimashedin51@gmail.com>
2021-12-08 22:03:06 +0200

    Added some tests

--- src/test/java/com/nure/kuib19/BaseTest.java
+++ src/test/java/com/nure/kuib19/BaseTest.java
@@ -3,12 +3,14 @@ package com.nure.kuib19;
 import com.nure.kuib19.driver.DriverManager;
 import com.nure.kuib19.pages.BasePage;
 import com.nure.kuib19.pages.HomePage;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
 public class BaseTest {
+    private static final String cityName = "Николаев";
+    private static final String attentionText = "Зміни в умовах доставки! Вартість доставки складає 15грн в межах 1 км, за кожний наступний км+10грн. Кінцеву вартість доставки уточнюйте у оператора";
+
+
     SoftAssert softAssert = new SoftAssert();
     protected final BasePage basePage = new BasePage();
     protected final HomePage homePage = new HomePage();
@@ -17,6 +19,13 @@ public class BaseTest {
     public void createSession() {
         DriverManager.getDriver();
         basePage.clickAttentionButton();
+        softAssert.assertTrue(homePage.checkCityButton());
+        homePage.clickCity(cityName);
+        softAssert.assertTrue(homePage.checkAttentionContinueButton());
+        homePage.clickAttentionContinueButton();
+        softAssert.assertTrue(homePage.checkAttentionText(attentionText));
+        homePage.clickAttentionOkButton();
+        softAssert.assertAll();
     }
 
     /**
--- src/test/java/com/nure/kuib19/HomePageTest.java
+++ src/test/java/com/nure/kuib19/HomePageTest.java
@@ -2,15 +2,32 @@ package com.nure.kuib19;
 
 import org.testng.annotations.Test;
 
-public class HomePageTest extends BaseTest{
+public class HomePageTest extends BaseTest {
 
     @Test
-    public void test(){
-        String cityName = "Николаев";
-        softAssert.assertTrue(homePage.checkCityButton());
-        softAssert.assertNotNull(homePage.clickCity(cityName));
-        homePage.checkAttentionContinueButton();
-        homePage.clickAttentionContinueButton();
+    public void checkStartMenu() {
+        String menuItem = "POKE L";
+        String productTitle = "Poke L Збери сам";
+        softAssert.assertTrue(homePage.checkMenuItem(menuItem));
+        homePage.clickMenuItem(menuItem);
+        softAssert.assertTrue(homePage.isTitlePresent(productTitle));
+        softAssert.assertAll();
+    }
+
+    @Test(description = "Check types of delivery")
+    public void checkTypesOfDelivery() {
+        String firstNameOfDelivery = "Delivery";
+        String secondNameOfDelivery = "Pickup";
+        String pickupAddress = "PokeWay_Соборная";
+        softAssert.assertTrue(homePage.checkTextTypeOfDeliveryButton(secondNameOfDelivery));
+        homePage.clickTypeOfDeliveryPassiveButton();
+        basePage.clickBackButton();
+        softAssert.assertTrue(homePage.checkTextTypeOfDeliveryButton(secondNameOfDelivery));
+        homePage.clickTypeOfDeliveryPassiveButton();
+        homePage.selectPickUpAddress(pickupAddress);
+        softAssert.assertTrue(homePage.getSelectedAddressText().contains(secondNameOfDelivery));
+        softAssert.assertTrue(homePage.checkTextTypeOfDeliveryButton(firstNameOfDelivery));
+        homePage.selectPickUpAddress(firstNameOfDelivery);
         softAssert.assertAll();
     }
 }
